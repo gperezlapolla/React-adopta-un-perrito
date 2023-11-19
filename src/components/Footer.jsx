c0c00cc440d4d9e04a8f9c5b6a1ad1412c5032d0
Guillermo Perez Lapolla <141288383+gperezlapolla@users.noreply.github.com>
2023-11-18 23:52:28 -0300

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Esta galería de imágenes muestra maravillosos perritos que necesitan ser adoptados.
+        Puedes ayudar a encontrarles un hogar amoroso.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
+
